20d0c5f9bc1c9c64f9111a7821d8993391e65a66
Kyle Granat <kyle@trossenrobotics.com>
2016-08-01 11:16:34 -0500

    Example fix

--- libraries/ArbotiX/examples/arbotix/arbotiXBlink/arbotiXBlink.ino
+++ libraries/ArbotiX/examples/arbotix/arbotiXBlink/arbotiXBlink.ino
@@ -10,13 +10,13 @@
 // the setup routine runs once when you press reset:
 void setup() {                
   // initialize the digital pin as an output.
-  pinMode(USER_LED, OUTPUT);   // Pin 0 maps to the USER LED on the ArbotiX Robocontroller. Defined as USER_LED in the library  
+  pinMode(0, OUTPUT);   // Pin 0 maps to the USER LED on the ArbotiX Robocontroller.   
 }
 
 // the loop routine runs over and over again forever:
 void loop() {
-  digitalWrite(USER_LED, HIGH);   // turn the LED on (HIGH is the voltage level)
+  digitalWrite(0, HIGH);   // turn the LED on (HIGH is the voltage level)
   delay(1000);               // wait for a second
-  digitalWrite(USER_LED, LOW);    // turn the LED off by making the voltage LOW
+  digitalWrite(0, LOW);    // turn the LED off by making the voltage LOW
   delay(1000);               // wait for a second
 }
