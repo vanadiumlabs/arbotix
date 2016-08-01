75c42778491eb07dcbe49cadd26f680cee31a274
Kyle Granat <kyle@trossenrobotics.com>
2016-07-31 22:52:16 -0500

    moving libraries

--- libraries/ArbotiX/examples/delete/tests/AXTurretTest/poses.h
+++
@@ -1,9 +0,0 @@
-#ifndef POSES
-#define POSES
-
-#include <avr/pgmspace.h>
-
-const PROGMEM uint16_t Center[] = {2, 512, 512};
-const PROGMEM uint16_t Home[] = {2, 512, 512};
-
-#endif
---
+++ libraries/Bioloid/examples/experimental/setPoseWidow/poses.h
@@ -0,0 +1,23 @@
+#ifndef POSES
+#define POSES
+
+#include <avr/pgmspace.h>
+//
+const PROGMEM uint16_t center[] = {6, 2048, 2048, 2048, 2047, 512, 512};
+const PROGMEM uint16_t pose1[] =  {6, 1023, 1023, 1448, 2336, 256, 256};
+
+//const PROGMEM uint16_t center[] = {6, 1800, 1800, 1800, 1800, 512, 512};
+//const PROGMEM uint16_t pose1[] =  {6, 1800, 2000, 2000, 2000, 256, 256};
+
+
+//const PROGMEM uint16_t center[] = {2, 2048, 1024};
+//const PROGMEM uint16_t pose1[] =  {2, 2033, 1024 };
+
+//const PROGMEM uint16_t center[] = {2, 2048, 2048};
+//const PROGMEM uint16_t pose1[] =  {2, 0, 1500 };
+
+//const PROGMEM uint16_t center[] = {2, 2047, 2046};
+//const PROGMEM uint16_t pose1[] =  {2, 1023, 1023 };
+
+
+#endif
